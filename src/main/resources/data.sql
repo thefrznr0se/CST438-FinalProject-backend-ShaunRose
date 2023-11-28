3c51e0b74f20b20ef6f0d351521783487483ee8e
Aaron Martinez <amartinezloera@csumb.edu>
2023-11-28 10:22:18 -0800

    made changes to the database and fixed errors and updated unit test to work with database changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,9 +11,9 @@ INSERT INTO Price (price) VALUES
 
 -- Insert data into Movies table
 INSERT INTO Movie (movie_title, movie_rating, movie_length, price_id) VALUES
-('Movie A', 'PG-13', 120, 1),
-('Movie B', 'R', 150, 2),
-('Movie C', 'PG', 105, 3);
+('Oppenheimer', 'PG-13', 120, 1),
+('Barbie', 'R', 150, 2),
+('Hidden Figures', 'PG', 105, 3);
 
 -- Insert data into Rooms table
 INSERT INTO Room (capacity) VALUES
@@ -30,15 +30,15 @@ INSERT INTO Schedule (start_time, end_time, date, movie_id, room_id) VALUES
 
 -- Insert additional data into Movies table
 INSERT INTO Movie (movie_title, movie_rating, movie_length, price_id) VALUES
-('Movie D', 'PG', 110, 1),
-('Movie E', 'R', 130, 2),
-('Movie F', 'PG-13', 95, 3),
-('Movie G', 'PG', 125, 1),
-('Movie H', 'R', 140, 2),
-('Movie I', 'PG-13', 100, 3),
-('Movie J', 'PG', 115, 1),
-('Movie K', 'R', 160, 2),
-('Movie L', 'PG-13', 90, 3);
+('Alice in Wonderland', 'G', 110, 1),
+('The Grinch', 'R', 130, 2),
+('Interstellar', 'PG-13', 95, 3),
+('Inside Out', 'PG', 125, 1),
+('Finding Nemo', 'R', 140, 2),
+('Iron Man', 'PG-13', 100, 3),
+('Batman', 'PG', 115, 1),
+('Harry Potter and the Goblet of Fire', 'R', 160, 2),
+('Cars', 'PG-13', 90, 3);
 
 -- Insert additional data into Schedule table
 INSERT INTO Schedule (start_time, end_time, date, movie_id, room_id) VALUES

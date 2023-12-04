a55422656879c936fe715b20c1c4a7ddd2c37a70
Adrian <adrianmartinez1@csumb.edu>
2023-12-04 02:11:54 -0800

    Added basic implementation for AccountCredentials

--- src/main/java/com/cst438/dto/AccountCredentials.java
+++ src/main/java/com/cst438/dto/AccountCredentials.java
@@ -1,5 +1,28 @@
 package com.cst438.dto;
 
-public record AccountCredentials(String username, String password) {
-	
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+
+    public AccountCredentials(String username, String password) {
+        if (username == null || username.length() < 4 || username.length() > 255) {
+            throw new IllegalArgumentException("Invalid username");
+        }
+        if (password == null || password.length() < 8) {
+            throw new IllegalArgumentException("Invalid password");
+        }
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
+
+// O
